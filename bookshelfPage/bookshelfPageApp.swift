695ba5d0441d207dae7ad29d3b8246debecd48b5
Karmen <karmengeisel@icloud.com>
2023-06-28 13:24:38 -0600

    Initial Commit

---
+++ bookshelfPage/bookshelfPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookshelfPageApp.swift
+//  bookshelfPage
+//
+//  Created by Scholar on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct bookshelfPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
